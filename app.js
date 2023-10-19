fe2198c5a62ac35463c9132e48eea100972cec97
Abdullah Kılıçaslan <abdullahkilicaslan.ka@gmail.com>
2023-10-19 16:46:42 +0300

    selam

--- app.js
+++ app.js
@@ -1,31 +1,71 @@
 const { request } = require("express");
 const express = require(`express`)
-
-
+const mongoose = require("mongoose");
+const pageRoute = require("./routes/pageRoute")
+const courseRoute = require('./routes/courseRoute')
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute"); 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+var flash = require('connect-flash');
+const methodOverride = require('method-override');
 
 
 const app = express();
-const port = 3000;
+//Connect DB
+mongoose
+  .connect("mongodb+srv://apokaslan:OHi9a12xFtpPsash@cluster0.ryjpyx6.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useFindAndModify: false,
+    //useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB Connected Successfully');
+  });
+
 // template engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+
+
+//global variablw
+global.userIN = null;
+
 
 //middleware 
-app.use(express.static("public"));
 
-app.get("/",  (req,res) =>{
-    res.render("index",{
-        page_name: "index"
-    });
-        
-})
+app.use(express.static('public'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-app.get("/about", (req , res) =>{
-    res.render("about",{
-        page_name: "about"
-    })
-})
-
-
-app.listen (port, () =>{
-    console.log(`App started on port  ${port}`)
+//Routes
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
 });
---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,13 @@
+const User = require('../models/User');
+
+
+
+module.exports = async(req, res,next) => {
+  const user = await User.findById(req.session.userID);
+  if(!user){
+    res.redirect('/login');
+    
+  }
+  return next();
+  
+}
